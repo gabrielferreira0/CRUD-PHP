90fd1b172c3ed725c206bec4e44accbf9b1ddb5d
Gabriel Ferreira <47874487+KKAEL1998@users.noreply.github.com>
2020-12-11 11:25:59 -0300

    Att

--- Crud/docs/js/teste.js
+++ Crud/docs/js/teste.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
     CPF.mask('999.999.999-99');
 
     $('#teste').on('click', '#cadastrar', function () {
-        $('#alerta').show().fadeOut(3000);
         let Username = $("#Username").val();
         let Senha = $("#Senha").val();
         let Email = $("#Email").val();
@@ -11,6 +10,29 @@ $(document).ready(function () {
         let Nascimento = $("#dt-nascimento").val();
         let Cidade = $("#Cidade").val();
         console.log("Username: " + Username + " Senha: " + Senha + " Email: " + Email + " CPF: " + CPF + " Nascimento: " + Nascimento + " Cidade: " + Cidade);
+        let url = 'Index.php';
+        $.ajax({
+            type: 'post',
+            dataType: 'json',
+            url: url,
+            async: true,
+            data: {
+                rq: 'cadastrar',
+                Username: Username,
+                Senha: Senha,
+                Email: Email,
+                CPF: CPF,
+                Nascimento: Nascimento,
+                Cidade: Cidade
+            },
+            success: function (response) {
+                $('#alerta').show().fadeOut(3000);
+            },
+            error: function (e) {
+                bootbox.alert("<h2>Erro :(</h2><br/><center>Não foi possivel realizar essa operação.</center></br>");
+            }
+        });
+
     });
 
 });
